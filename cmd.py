ddc37b5bc73bf9f6cb02658511314bb5a70847b5
Conway <yawnoc@pm.me>
2022-04-02 21:06:14 +0800

    Implement file not found error messages

--- cmd.py
+++ cmd.py
@@ -11,6 +11,10 @@ Licensed under "MIT No Attribution" (MIT-0), see LICENSE.
 
 import argparse
 import re
+import sys
+
+
+COMMAND_LINE_ERROR_EXIT_CODE = 2
 
 
 def to_normalised_name(cmd_file_name):
@@ -22,12 +26,37 @@ def to_normalised_name(cmd_file_name):
   Here we normalise it to `name`.
   """
   
-  cmd_name = re.sub(r'\.(cmd)?\Z', '', cmd_file_name)
+  normalised_cmd_name = re.sub(r'\.(cmd)?\Z', '', cmd_file_name)
   
-  return cmd_name
+  return normalised_cmd_name
 
 
-def generate_html_file(cmd_file_name):
+def generate_html_file(cmd_file_name, is_direct_command_line_call):
+  
+  normalised_cmd_name = to_normalised_name(cmd_file_name)
+  normalised_cmd_file_name = f'{normalised_cmd_name}.cmd'
+  
+  try:
+    with open(normalised_cmd_name, 'r', encoding='utf-8') as cmd_file:
+      cmd_file_content = cmd_file.read()
+  except FileNotFoundError as file_not_found_error:
+    if is_direct_command_line_call:
+      error_message = \
+              (
+                'error: '
+                f'argument `{cmd_file_name}`: '
+                f'file `{normalised_cmd_file_name}` not found'
+              )
+      print(error_message)
+      sys.exit(COMMAND_LINE_ERROR_EXIT_CODE)
+    else:
+      error_message = \
+              (
+                f'file `{normalised_cmd_file_name}` not found '
+                f'for `{cmd_file_name}` in cmd_file_name_list'
+              )
+      raise FileNotFoundError(error_message) from file_not_found_error
+  
   return # TODO: implement this properly
 
 
@@ -61,12 +90,12 @@ def main():
   cmd_file_name = parsed_arguments.cmd_file_name
   
   if cmd_file_name != '':
-    generate_html_file(cmd_file_name)
+    generate_html_file(cmd_file_name, is_direct_command_line_call=True)
     return
   
   cmd_file_name_list = [] # TODO: implement this properly
   for cmd_file_name in cmd_file_name_list:
-    generate_html_file(cmd_file_name)
+    generate_html_file(cmd_file_name, is_direct_command_line_call=False)
 
 
 if __name__ == '__main__':
