77fb6a8c5f960f619ecfd5ba633b8015076132ba
Conway <yawnoc@pm.me>
2022-04-07 21:49:48 +0800

    Use double-angle quotes for readable docstring variables

--- cmd.py
+++ cmd.py
@@ -76,14 +76,14 @@ def extract_cmd_name(cmd_file_name_argument):
   Extract name-without-extension from a CMD file name argument.
   
   The argument received from the command line can be either
-  `{cmd_name}.cmd`, `{cmd_name}.`, or `{cmd_name}`,
-  or even `./{cmd_name}.cmd` if the user is being dumb.
+  `«cmd_name».cmd`, `«cmd_name».`, or `«cmd_name»`,
+  or even `./«cmd_name».cmd` if the user is being dumb.
   If no argument be received from the command line,
-  we loop through the file names, which are `./{cmd_name}.cmd`.
+  we loop through the file names, which are `./«cmd_name».cmd`.
   If the operating system be Windows,
   the directory separators will be backslashes
   instead of forward slashes (as in URLs).
-  In all cases we want `{cmd_name}` with forward slashes.
+  In all cases we want `«cmd_name»` with forward slashes.
   """
   
   cmd_file_name_argument = re.sub(r'\\', '/', cmd_file_name_argument)
