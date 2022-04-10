1c98c7ccd92da3c6198db2e2ebd1b30c9c57b8fa
Conway <yawnoc@pm.me>
2022-04-10 11:22:36 +0800

    Refactor process class declaration to method

--- cmd.py
+++ cmd.py
@@ -334,6 +334,36 @@ class ReplacementMaster:
       insertion_index = None
     self._replacement_queue.insert(insertion_index, replacement)
   
+  def process_class_declaration(
+    self,
+    class_declaration_match,
+    source_file,
+    line_number,
+  ):
+    
+    class_name = get_group('class_name', class_declaration_match)
+    id_ = get_group('id_', class_declaration_match)
+    
+    if class_name == 'ExtensibleFenceReplacement':
+      replacement = ExtensibleFenceReplacement(id_)
+    else:
+      print(
+        'error: '
+        f'{source_file} line {line_number}: '
+        f'unrecognised replacement class `{class_name}`'
+      )
+      sys.exit(GENERIC_ERROR_EXIT_CODE)
+
+    if id_ in self._replacement_from_id:
+      print(
+        'error: '
+        f'{source_file} line {line_number}: '
+        f'replacement already declared with id `{id_}'
+      )
+      sys.exit(GENERIC_ERROR_EXIT_CODE)
+    
+    return class_name, replacement
+  
   def legislate(self, replacement_rules, source_file):
     
     class_name = None
@@ -356,30 +386,15 @@ class ReplacementMaster:
       class_declaration_match = \
               ReplacementMaster.compute_class_declaration_match(line)
       if class_declaration_match is not None:
-        
         if replacement is not None:
           self.commit(replacement, source_file, line_number, class_name)
-        
-        class_name = get_group('class_name', class_declaration_match)
-        id_ = get_group('id_', class_declaration_match)
-        
-        if class_name == 'ExtensibleFenceReplacement':
-          replacement = ExtensibleFenceReplacement(id_)
-        else:
-          print(
-            'error: '
-            f'{source_file} line {line_number}: '
-            f'unrecognised replacement class `{class_name}`'
-          )
-          sys.exit(GENERIC_ERROR_EXIT_CODE)
-        
-        if id_ in self._replacement_from_id:
-          print(
-            'error: '
-            f'{source_file} line {line_number}: '
-            f'replacement already declared with id `{id_}'
-          )
-          sys.exit(GENERIC_ERROR_EXIT_CODE)
+        class_name, replacement = \
+                self.process_class_declaration(
+                  class_declaration_match,
+                  source_file,
+                  line_number,
+                )
+        continue
       
       # TODO: other cases
     
