7e33a774cdb63c53ebcba17676cc3db5a15d9dc3
Conway <yawnoc@pm.me>
2022-04-16 23:19:45 +0800

    Improve readability of extract_cmd_name regex pattern

--- cmd.py
+++ cmd.py
@@ -2247,7 +2247,16 @@ def extract_cmd_name(cmd_file_name_argument):
   """
   
   cmd_file_name_argument = os.path.normpath(cmd_file_name_argument)
-  cmd_name = re.sub(r'[.](cmd)?\Z', '', cmd_file_name_argument)
+  cmd_name = \
+          re.sub(
+            r'''
+              [.] (cmd)?
+              \Z
+            ''',
+            '',
+            cmd_file_name_argument,
+            flags=re.VERBOSE,
+          )
   
   return cmd_name
 
