conwaymd/conwaymd

commit 9e02ac849b5d9aa2171bc8020d057c12704b222c

Implement #display-code [Broken attribute specifications]

cmd.py
@@ -2649,8 +2649,29 @@ ExtensibleFenceReplacement: #literals
     #placeholder-protect
 - closing_delimiter: >
 
-ReplacementSequence: #whitespace
+RegexDictionaryReplacement: #code-wrap
+* \A --> <code>
+* \Z --> </code>
+
+ExtensibleFenceReplacement: #display-code
 - queue_position: AFTER #literals
+- syntax_type: BLOCK
+- allowed_flags:
+    u=KEEP_HTML_UNESCAPED
+    i=KEEP_INDENTED
+    w=REDUCE_WHITESPACE
+- extensible_delimiter: ``
+- attribute_specifications: EMPTY
+- content_replacements:
+    #escape-html
+    #de-indent
+    #reduce-whitespace
+    #code-wrap
+    #placeholder-protect
+- tag_name: pre
+
+ReplacementSequence: #whitespace
+- queue_position: AFTER #display-code
 - replacements:
     #reduce-whitespace
 

commit 0fe12ddc6091d392883b9905e1b2e93569c4d68b

Fix missing return for attribute_specifications EMPTY
which caused bug in 9e02ac8

cmd.py
@@ -1330,6 +1330,7 @@ class ReplacementMaster:
     
     if attribute_specifications_match.group('empty_keyword') is not None:
       replacement.attribute_specifications = ''
+      return
     
     attribute_specifications = \
             attribute_specifications_match.group('attribute_specifications')