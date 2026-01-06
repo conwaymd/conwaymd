befd099748b09a52ae4167910894ac44773c312a
Conway <yawnoc@pm.me>
2026-01-06 16:51:32 +0800

    Revert "Add attribute href_replacements to ExplicitLinkReplacement"
    This reverts commit 72bdcd6103b21a6ec79731433d53add5c3b56240.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,7 +3,6 @@
 
 ## [Unreleased]
 
-- Added attribute `href_replacements` to `ExplicitLinkReplacement`
 - Converted GitHub actions `packages_dir` to kebab case
 
 
--- conwaymd/employables.py
+++ conwaymd/employables.py
@@ -16,7 +16,6 @@ from conwaymd.bases import (
     ReplacementWithAttributeSpecifications,
     ReplacementWithConcludingReplacements,
     ReplacementWithContentReplacements,
-    ReplacementWithHrefReplacements,
     ReplacementWithProhibitedContent,
     ReplacementWithSubstitutions,
     ReplacementWithSyntaxType,
@@ -1588,7 +1587,6 @@ class ReferencedImageReplacement(
 class ExplicitLinkReplacement(
     ReplacementWithAllowedFlags,
     ReplacementWithAttributeSpecifications,
-    ReplacementWithHrefReplacements,
     ReplacementWithContentReplacements,
     ReplacementWithConcludingReplacements,
     Replacement,
@@ -1602,7 +1600,6 @@ class ExplicitLinkReplacement(
     - queue_position: (def) NONE | ROOT | BEFORE #«id» | AFTER #«id»
     - allowed_flags: (def) NONE | «letter»=«FLAG_NAME» [...]
     - attribute_specifications: (def) NONE | EMPTY | «string»
-    - href_replacements: (def) NONE | #«id» [...]
     - content_replacements: (def) NONE | #«id» [...]
     - concluding_replacements: (def) NONE | #«id» [...]
     ````
@@ -1621,7 +1618,6 @@ class ExplicitLinkReplacement(
             'queue_position',
             'allowed_flags',
             'attribute_specifications',
-            'href_replacements',
             'content_replacements',
             'concluding_replacements',
         )
@@ -1676,10 +1672,7 @@ class ExplicitLinkReplacement(
             enabled_flag_names = ReplacementWithAllowedFlags.get_enabled_flag_names(match,
                                                                                     flag_name_from_letter, has_flags)
 
-            content = href = match.group('uri')
-            for replacement in self._href_replacements:
-                href = replacement.apply(href, enabled_flag_names)
-
+            href = match.group('uri')
             href_protected = PlaceholderMaster.protect(href)
             href_attribute_specification = f'href={href_protected}'
 
@@ -1694,6 +1687,7 @@ class ExplicitLinkReplacement(
 
             attributes_sequence = build_attributes_sequence(combined_attribute_specifications, use_protection=True)
 
+            content = href
             for replacement in self._content_replacements:
                 content = replacement.apply(content, enabled_flag_names)
 
